65d9b1526c5bedf510e1d5f20dc2536ecebba8e3
nozim-dev <nozimfordevelop@gmail.com>
2025-03-15 16:18:20 +0500

    kamchiliklar to'g'irlandi.

--- js/script.js
+++ js/script.js
@@ -38,7 +38,11 @@ button.addEventListener("click", function () {
         };
         reader.readAsDataURL(fileInput.files[0]);
     } else {
-        Qrcode(urlInput.value, "#000", "#fff", false, "../img/logo-big.png");
+        if (window.screen.availWidth <= 380) {
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#000", "#fff", false, "../img/logo-big.png", 200, 200);
+        } else {
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#000", "#fff", false, "../img/logo-big.png", 280, 280);
+        }
     }
 });
 // ** Oq rang tugmasi bosilganda**
@@ -58,9 +62,9 @@ document.querySelector(".white").addEventListener("click", function () {
         reader.readAsDataURL(fileInput.files[0]);
     } else {
         if (window.screen.availWidth <= 380) {
-            Qrcode(urlInput.value, "#fff", "#000", false, "../img/logo-big.png", 200, 200);
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#fff", "#000", false, "../img/logo-big.png", 200, 200);
         } else {
-            Qrcode(urlInput.value, "#fff", "#000", false, "../img/logo-big.png", 280, 280);
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#fff", "#000", false, "../img/logo-big.png", 280, 280);
         }
     }
     document.querySelector(".main_box_qrcode").style.backgroundColor = "#000";
@@ -82,9 +86,9 @@ document.querySelector(".black").addEventListener("click", function () {
         reader.readAsDataURL(fileInput.files[0]);
     } else {
         if (window.screen.availWidth <= 380) {
-            Qrcode(urlInput.value, "#000", "#fff", false, "../img/logo-big.png", 200, 200);
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#000", "#fff", false, "../img/logo-big.png", 200, 200);
         } else {
-            Qrcode(urlInput.value, "#000", "#fff", false, "../img/logo-big.png", 280, 280);
+            Qrcode("https://qrcode-generator-xi-one.vercel.app/", "#000", "#fff", false, "../img/logo-big.png", 280, 280);
         }
     }
     document.querySelector(".main_box_qrcode").style.backgroundColor = "#fff";
